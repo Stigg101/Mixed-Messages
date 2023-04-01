a22cc9285fc3738670a9824edfc3c4883bb67d22
Stigg101 <gustavheese@live.com>
2023-04-01 08:06:20 +0200

    added user input to use the name

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const adjectivesData = require('./adjectives.json');
 
 // Define the generateMessage function using arrow notation
 const generateMessage = () => {
+    // Prompt the user for their name using the prompt() function
+    const name = prompt("What is your name?");
     // Choose a random sport, action, and adjective from the imported data using Math.random() and Math.floor()
     const sport = sportsData.sports[Math.floor(Math.random() * sportsData.sports.length)];
     const action = actionsData.actions[Math.floor(Math.random() * actionsData.actions.length)];
     const adjective = adjectivesData.adjectives[Math.floor(Math.random() * adjectivesData.adjectives.length)];
     // Return a string that combines the chosen sport, action, and adjective into a motivational message
-    return `You are ${adjective} at ${sport}, ${action}!`
+    return `${name}, you are ${adjective} at ${sport}, ${action}!`
 };
 
 // Call the generateMessage function and output the result to the console
