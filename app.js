a5fe259750c304f529b890d91eaca6396c1c61c1
Stigg101 <gustavheese@live.com>
2023-04-01 08:01:31 +0200

    Added comments to improve readability

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+// Import the data from the JSON files using the require() function
 const sportsData = require('./sport.json');
 const actionsData = require('./actions.json');
 const adjectivesData = require('./adjectives.json');
 
+// Define the generateMessage function using arrow notation
 const generateMessage = () => {
+    // Choose a random sport, action, and adjective from the imported data using Math.random() and Math.floor()
     const sport = sportsData.sports[Math.floor(Math.random() * sportsData.sports.length)];
     const action = actionsData.actions[Math.floor(Math.random() * actionsData.actions.length)];
     const adjective = adjectivesData.adjectives[Math.floor(Math.random() * adjectivesData.adjectives.length)];
+    // Return a string that combines the chosen sport, action, and adjective into a motivational message
     return `You are ${adjective} at ${sport}, ${action}!`
 };
 
+// Call the generateMessage function and output the result to the console
 console.log(generateMessage());
